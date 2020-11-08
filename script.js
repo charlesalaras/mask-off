7ff82ba71f348b26d66ac5e3027d9a0c7671a3b4
Jared.Tan <Jtanu45@gmail.com>
2020-11-07 17:28:03 -0800

    Big Changes
    Got heatmap working, editted format

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 let map;
 
 function initMap() {
+
+  var UCR = new google.maps.LatLng(33.9737055, -117.3280644);
+
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+    center: UCR,
+    zoom: 13,
   });
+
+  var heatmapData = [
+    {location: new google.maps.LatLng(33.9737055, -117.3280644), weight: 0.001},
+    new google.maps.LatLng(33.9737055, -117.3280644),
+    new google.maps.LatLng(34.9737055, -117.3280644),
+    new google.maps.LatLng(35.9737055, -117.3280644),
+    new google.maps.LatLng(36.9737055, -117.3280644),
+    new google.maps.LatLng(37.9737055, -117.3280644),
+    {location: new google.maps.LatLng(33.9737055, -116.3280644), weight: 1}
+  ];
+
+  var heatmap = new google.maps.visualization.HeatmapLayer({
+    data: heatmapData
+  });
+  heatmap.setMap(map);
 }
