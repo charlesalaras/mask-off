fd787e1fdbf3228ee385ad60f96a54d56545bf81
Jared.Tan <Jtanu45@gmail.com>
2020-11-07 19:57:04 -0800

    Update script.js

--- script.js
+++ script.js
@@ -16,6 +16,21 @@ function initMap() {
     new google.maps.LatLng(35.9737055, -117.3280644),
     new google.maps.LatLng(36.9737055, -117.3280644),
     new google.maps.LatLng(37.9737055, -117.3280644),
+	new google.maps.LatLng(37.9737055, -117.3280644),
+	new google.maps.LatLng(37.9737055, -117.3280644),
+	new google.maps.LatLng(33.9771143, -117.3395029),
+	new google.maps.LatLng(33.9771143, -117.3395029),
+	new google.maps.LatLng(33.9771143, -117.3395029),
+	new google.maps.LatLng(33.9771143, -117.3395029),
+	new google.maps.LatLng(33.9771143, -117.3395029),
+	new google.maps.LatLng(33.9771142, -117.3395025),
+	new google.maps.LatLng(33.9771142, -117.3395026),
+	new google.maps.LatLng(33.9771144, -117.3395026),
+	new google.maps.LatLng(33.9771144, -117.3395029),
+	new google.maps.LatLng(33.9771142, -117.3395028),
+	new google.maps.LatLng(33.9771143, -117.3395027),
+	new google.maps.LatLng(33.9771145, -117.3395027),
+	
     {location: new google.maps.LatLng(33.9737055, -116.3280644), weight: 1}
   ];
 
